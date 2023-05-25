1129f1bf68259581200b63457172ec20a2656d63
Sveta <bulochka0497@mail.ru>
2023-05-25 23:16:01 +0700

    квадрат числа

--- Lessons1/Program.cs
+++ Lessons1/Program.cs
@@ -1,1 +1,7 @@
 ﻿
+//Напишите программу, кторая на вход принимает число и
+//выдаёт его квадрат (число умноженное на само себя).
+
+double input = double.Parse(Console.ReadLine());
+
+Console.WriteLine(input * input);
